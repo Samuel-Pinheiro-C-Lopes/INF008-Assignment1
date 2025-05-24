31a708334ceada085a1d6c2fbd0d9ce1bb44ab01
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-05-24 18:50:11 -0300

    Intefaces created

---
+++ src/main/java/samuel/inf008/assignment1/interfaces/IEntity.java
@@ -0,0 +1,5 @@
+package samuel.inf008.assignment1.interfaces;
+
+public interface IEntity extends IPrintable {
+    public Integer getId();
+}
---
+++ src/main/java/samuel/inf008/assignment1/interfaces/IPrintable.java
@@ -0,0 +1,5 @@
+package samuel.inf008.assignment1.interfaces;
+
+public interface IPrintable {
+    public void printSelf();
+}
